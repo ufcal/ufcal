17fcf932812842c909ddca6344ee665de1ff6c3f
naoki hirata <naoki@magic3.org>
2025-04-14 11:39:27 +0900

    Tailwind v4にバージョンアップ。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import node from '@astrojs/node'
 import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 import holidayIntegration from './src/integrations/holiday'
@@ -8,10 +8,6 @@ import holidayIntegration from './src/integrations/holiday'
 export default defineConfig({
   integrations: [
     react(),
-    tailwind({
-      // デフォルトのベーススタイルを無効にする
-      applyBaseStyles: false
-    }),
     icon({
       include: {
         mdi: ['*']
@@ -26,6 +22,7 @@ export default defineConfig({
   }),
   server: { port: 3000, host: true },
   vite: {
+    plugins: [tailwindcss()],
     css: {
       preprocessorOptions: {
         scss: {
