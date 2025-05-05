b8828fd9852beefbd67a5f70f3e44ca0bfb10bd1
naoki hirata <naoki@magic3.org>
2025-05-05 17:18:35 +0900

    管理から抜けていたファイルを追加。

---
+++ src/fetch/member/password.ts
@@ -0,0 +1,17 @@
+import config from '@/config/config.json'
+
+class MemberPasswordFetch {
+  async updatePassword(
+    id: number,
+    data: { currentPassword: string; newPassword: string }
+  ): Promise<any> {
+    const response = await fetch(`${config.api.memberUrl}/password/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    })
+    return response
+  }
+}
+
+export default new MemberPasswordFetch()
