3c2cb69c4f0eb5a42e65b7c653d8b07e284f6041
naoki hirata <naoki@magic3.org>
2025-05-20 20:54:52 +0900

    イベントのカテゴリーのデータ型を文字列に変更。

--- src/config/master/category.ts
+++ src/config/master/category.ts
@@ -1,7 +1,7 @@
 export const colors = [
-  { name: 'デフォルト', color: '#16A34A' },
-  { name: 'カテゴリー1', color: '#3357FF' },
-  { name: 'カテゴリー2', color: '#FF33A1' },
-  { name: 'カテゴリー3', color: '#FF8C33' },
-  { name: 'カテゴリー4', color: '#FF5342' }
+  { name: 'デフォルト', value: '', color: '#16A34A' },
+  { name: 'カテゴリー1', value: 'category1', color: '#3357FF' },
+  { name: 'カテゴリー2', value: 'category2', color: '#FF33A1' },
+  { name: 'カテゴリー3', value: 'category3', color: '#FF8C33' },
+  { name: 'カテゴリー4', value: 'category4', color: '#FF5342' }
 ]
